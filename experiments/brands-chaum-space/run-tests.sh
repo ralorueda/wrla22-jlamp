baa92de6b206c64a2b0668aa05b83d72164df5a1
Raul Lopez <ralorueda@gmail.com>
2023-05-09 08:25:39 +0200

    update

--- experiments/brands-chaum-space/run-tests.sh
+++ experiments/brands-chaum-space/run-tests.sh
@@ -1,15 +1,15 @@
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/regular-execution-standard.maude > ./results/regular-execution-standard.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/regular-execution-standard.maude > ./results/regular-execution-standard.txt & disown
 sleep 2
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-no-const-standard.maude > ./results/mafia-no-const-standard.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-no-const-standard.maude > ./results/mafia-no-const-standard.txt & disown
 sleep 2
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-standard.maude > ./results/mafia-standard.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-standard.maude > ./results/mafia-standard.txt & disown
 sleep 2
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/hijacking-standard.maude > ./results/hijacking-standard.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/hijacking-standard.maude > ./results/hijacking-standard.txt & disown
 sleep 2
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/regular-execution-canonical.maude > ./results/regular-execution-canonical.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/regular-execution-canonical.maude > ./results/regular-execution-canonical.txt & disown
 sleep 2
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-no-const-canonical.maude > ./results/mafia-no-const-canonical.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-no-const-canonical.maude > ./results/mafia-no-const-canonical.txt & disown
 sleep 2
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-canonical.maude > ./results/mafia-canonical.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/mafia-canonical.maude > ./results/mafia-canonical.txt & disown
 sleep 2
-timeout 36000 maude-nra -no-prelude /pub/maude-a136/prelude.maude ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/hijacking-canonical.maude > ./results/hijacking-canonical.txt & disown
+timeout 36000 maude-nra ./../../smt-narrowing.maude ./brands-chaum-space.maude ./tests/hijacking-canonical.maude > ./results/hijacking-canonical.txt & disown
